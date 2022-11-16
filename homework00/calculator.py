65a7dcbc4dbc8ee438597109d27547bcdddc2655
lldckv <lldckv@gmail.com>
2022-11-17 02:39:38 +0300

    Исправление

--- homework00/calculator.py
+++ homework00/calculator.py
@@ -30,8 +30,10 @@ def match_case_calc_1(num: float, command: str):
 
 def match_case_calc_2(num_1: float, num_2: float, command: str) -> tp.Union[float, str]:  # type: ignore
     match command:
-        case "/" if num_2 != 0:
-            return num_1 / num_2
+        case "/":
+            if num_2 != 0:
+                return num_1 / num_2
+            return "Невозможно поделить на ноль"
         case "-":
             return num_1 - num_2
         case "+":
@@ -62,63 +64,70 @@ while T:
     print("Выберите тип операции:\nСправка : 0")
     print("Двуместная : 1\nОдноместная : 2\nВыход : 3")
     c = input("\n")
-    match c:
-        case "1":
-            print("Выберите тип операции:\nОдно действие : 0")
-            print("Более одного действия : 1\n")
-            c2 = input("\n")
-            match c2:
-                case "0":
-                    string = input("Введите выражение:" " символы через пробел, затем нажмите Enter: ")
-                    if check(string):
-                        n1, d, n2 = string.split(" ")
-                        print(string, "=", match_case_calc_2(float(n1), float(n2), d))
-                        T = False
-                    else:
-                        print(f"Некорректный ввод: {string!r}.")
-                case "1":
-                    pass
-                case _:
-                    pass
-        case "2":
-            print("Выберите тип операции:\nОдно действие : 0")
-            print("Более одного действия : 1\n")
-            c1 = input("\n")
-            match c1:
-                case "0":
-                    string = input("Введите выражение, затем нажмите Enter: ")
-                    if check(string):
-                        d, n1 = string.split("(")
-                        d = d.strip(")")
-                        n1 = n1.strip(")")
-                        print(string, "=", match_case_calc_1(float(n1), d))
-                        T = False
-                    else:
-                        print(f"Некорректный ввод: {string!r}.")
-                case "1":
-                    pass
-                case _:
-                    pass
-        case "0":
-            print("Калькулятор поддерживает : ", "\n")
-            print(
-                "Двуместные операции : ",
-                "Сложение, ввод: a + b",
-                "Вычитание, ввод: a - b",
-                "Деление, ввод: a / b",
-                "Умножение, ввод: a * b",
-                "Возведение в степень, ввод: a ** b",
-                "Перевод десятичного числа в систему счисления" " с основанием [2,9], ввод: a to b",
-                sep=("\n"),
-            )
-            print("\n")
-            print(
-                "Одноместные операции :\n Вычисление синуса, ввод: sin(a),\n "
-                "Вычисление косинуса, ввод: cos(a),\nВычисление тангенса, ввод: tan(a),"
-                "\nНатуральный логарифм, ввод: ln(a),\nДесятичный логарифм, ввод: lg(a)\n"
-            )
-            print("\n")
-        case "3":
-            break
-        case _:
-            print(f"Неизвестный оператор: {c!r}.")
+    while c != "3":
+        match c:
+            case "1":
+                print("Выберите тип операции:\nОдно действие : 0")
+                print("Отмена : 1\n")
+                c2 = input("\n")
+                match c2:
+                    case "0":
+                        string = input("Введите выражение: символы через пробел + Enter: ")
+                        if check(string):
+                            n1, d, n2 = string.split(" ")
+                            if not (n1.isdigit() and n2.isdigit()):
+                                print(f"Некорректный ввод: {string!r}.")
+                            else:
+                                print(string, "=", match_case_calc_2(float(n1), float(n2), d), "\n")
+                                T = False
+                        else:
+                            print(f"Некорректный ввод: {string!r}.")
+                    case "1":
+                        pass
+                    case _:
+                        print(f"Неизвестный оператор: {c2!r}.")
+            case "2":
+                print("Выберите тип операции:\nОдно действие : 0")
+                print("Отмена : 1\n")
+                c1 = input("\n")
+                match c1:
+                    case "0":
+                        string = input("Введите выражение + Enter: ")
+                        if check(string):
+                            d, n1 = string.split("(")
+                            d = d.strip(")")
+                            n1 = n1.strip(")")
+                            print(string, "=", match_case_calc_1(float(n1), d), "\n")
+                            T = False
+                        else:
+                            print(f"Некорректный ввод: {string!r}.")
+                    case "1":
+                        pass
+                    case _:
+                        print(f"Неизвестный оператор: {c1!r}.")
+            case "0":
+                print("Калькулятор поддерживает : ", "\n")
+                print(
+                    "Двуместные операции : ",
+                    "Сложение, ввод: a + b",
+                    "Вычитание, ввод: a - b",
+                    "Деление, ввод: a / b",
+                    "Умножение, ввод: a * b",
+                    "Возведение в степень, ввод: a ** b",
+                    "Перевод десятичного числа в систему счисления, ввод: a to b",
+                    sep=("\n"),
+                )
+                print("\n")
+                print(
+                    "Одноместные операции :\n Вычисление синуса, ввод: sin(a),\n "
+                    "Вычисление косинуса, ввод: cos(a),\nВычисление тангенса, ввод: tan(a),"
+                    "\nНатуральный логарифм, ввод: ln(a),\nДесятичный логарифм, ввод: lg(a)\n"
+                )
+                print("\n")
+            case "3":
+                break
+            case _:
+                print(f"Неизвестный оператор: {c!r}.")
+        print("Выберите тип операции:\nСправка : 0")
+        print("Двуместная : 1\nОдноместная : 2\nВыход : 3")
+        c = input("\n")
