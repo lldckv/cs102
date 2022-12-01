9ec44795eeca3d7bed33701498eeb8d8ade03bcf
lldckv <lldckv@gmail.com>
2022-12-01 20:51:57 +0300

    decrypt() done with %

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -15,16 +15,11 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     ciphertext = ""
     for i in range(len(plaintext)):
-        if plaintext[i].islower() and plaintext[i].isalpha():
-            if (ord(plaintext[i]) + shift) > ord("z"):
-                ciphertext += chr(((ord(plaintext[i]) + shift) % ord("z")) + (ord("a")) - 1)
+        if plaintext[i].isalpha():
+            if plaintext[i].islower():
+                ciphertext += chr(ord("z") - (ord("z") - ord(plaintext[i]) - shift) % (ord("z") - (ord("a")) + 1))
             else:
-                ciphertext += chr(ord(plaintext[i]) + shift)
-        elif plaintext[i].isupper() and plaintext[i].isalpha():
-            if (ord(plaintext[i]) + shift) > ord("Z"):
-                ciphertext += chr(((ord(plaintext[i]) + shift) % ord("Z")) + (ord("A")) - 1)
-            else:
-                ciphertext += chr(ord(plaintext[i]) + shift)
+                ciphertext += chr(ord("Z") - (ord("Z") - ord(plaintext[i]) - shift) % (ord("Z") - (ord("A")) + 1))
         else:
             ciphertext += plaintext[i]
     return ciphertext
@@ -44,16 +39,11 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     plaintext = ""
     for i in range(len(ciphertext)):
-        if ciphertext[i].islower() and ciphertext[i].isalpha():
-            if (ord(ciphertext[i]) - shift) < ord("a"):
-                plaintext += chr(ord("z") + 1 - (ord("a") - (ord(ciphertext[i]) - shift)))
+        if ciphertext[i].isalpha():
+            if ciphertext[i].islower():
+                plaintext += chr(ord("z") - (ord("z") - ord(ciphertext[i]) + shift) % (ord("z") - (ord("a")) + 1))
             else:
-                plaintext += chr(ord(ciphertext[i]) - shift)
-        elif ciphertext[i].isupper() and ciphertext[i].isalpha():
-            if (ord(ciphertext[i]) - shift) < ord("A"):
-                plaintext += chr(ord("Z") + 1 - (ord("A") - (ord(ciphertext[i]) - shift)))
-            else:
-                plaintext += chr(ord(ciphertext[i]) - shift)
+                plaintext += chr(ord("Z") - (ord("Z") - ord(ciphertext[i]) + shift) % (ord("Z") - (ord("A")) + 1))
         else:
             plaintext += ciphertext[i]
     return plaintext
