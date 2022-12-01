a2ad7044d3ffd160165bb7709a51f4c09a970c48
lldckv <lldckv@gmail.com>
2022-12-01 15:48:02 +0300

    added vigenere

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,3 +1,6 @@
+"I'm a little string doc!"
+
+
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
@@ -9,7 +12,22 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for i in range(len(plaintext) - len(keyword)):
+        keyword += keyword[i]
+    keyword = keyword.upper()
+    for i in range(len(plaintext)):
+        if plaintext[i].islower() and plaintext[i].isalpha():
+            if (ord(plaintext[i]) + (ord(keyword[i]) - ord("A"))) > ord("z"):
+                ciphertext += chr(((ord(plaintext[i]) + (ord(keyword[i]) - ord("A"))) % ord("z")) + (ord("a")) - 1)
+            else:
+                ciphertext += chr(ord(plaintext[i]) + (ord(keyword[i]) - ord("A")))
+        elif plaintext[i].isupper() and plaintext[i].isalpha():
+            if (ord(plaintext[i]) + (ord(keyword[i]) - ord("A"))) > ord("Z"):
+                ciphertext += chr(((ord(plaintext[i]) + (ord(keyword[i]) - ord("A"))) % ord("Z")) + (ord("A")) - 1)
+            else:
+                ciphertext += chr(ord(plaintext[i]) + (ord(keyword[i]) - ord("A")))
+        else:
+            ciphertext += plaintext[i]
     return ciphertext
 
 
@@ -24,5 +42,20 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for i in range(len(ciphertext) - len(keyword)):
+        keyword += keyword[i]
+    keyword = keyword.upper()
+    for i in range(len(ciphertext)):
+        if ciphertext[i].islower() and ciphertext[i].isalpha():
+            if (ord(ciphertext[i]) - (ord(keyword[i]) - ord("A"))) < ord("a"):
+                plaintext += chr(ord("z") + 1 - (ord("a") - (ord(ciphertext[i]) - (ord(keyword[i]) - ord("A")))))
+            else:
+                plaintext += chr(ord(ciphertext[i]) - (ord(keyword[i]) - ord("A")))
+        elif ciphertext[i].isupper() and ciphertext[i].isalpha():
+            if (ord(ciphertext[i]) - (ord(keyword[i]) - ord("A"))) < ord("A"):
+                plaintext += chr(ord("Z") + 1 - (ord("A") - (ord(ciphertext[i]) - (ord(keyword[i]) - ord("A")))))
+            else:
+                plaintext += chr(ord(ciphertext[i]) - (ord(keyword[i]) - ord("A")))
+        else:
+            plaintext += ciphertext[i]
     return plaintext
