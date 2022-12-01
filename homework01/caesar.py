f3531dc46b51b4494540d0f0b8cac8e957a3aa63
lldckv <lldckv@gmail.com>
2022-12-01 15:44:01 +0300

    added homework01 caesar

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,3 +1,6 @@
+"I'm a little string doc!"
+
+
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
@@ -11,7 +14,19 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    for i in range(len(plaintext)):
+        if plaintext[i].islower() and plaintext[i].isalpha():
+            if (ord(plaintext[i]) + shift) > ord("z"):
+                ciphertext += chr(((ord(plaintext[i]) + shift) % ord("z")) + (ord("a")) - 1)
+            else:
+                ciphertext += chr(ord(plaintext[i]) + shift)
+        elif plaintext[i].isupper() and plaintext[i].isalpha():
+            if (ord(plaintext[i]) + shift) > ord("Z"):
+                ciphertext += chr(((ord(plaintext[i]) + shift) % ord("Z")) + (ord("A")) - 1)
+            else:
+                ciphertext += chr(ord(plaintext[i]) + shift)
+        else:
+            ciphertext += plaintext[i]
     return ciphertext
 
 
@@ -28,5 +43,17 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for i in range(len(ciphertext)):
+        if ciphertext[i].islower() and ciphertext[i].isalpha():
+            if (ord(ciphertext[i]) - shift) < ord("a"):
+                plaintext += chr(ord("z") + 1 - (ord("a") - (ord(ciphertext[i]) - shift)))
+            else:
+                plaintext += chr(ord(ciphertext[i]) - shift)
+        elif ciphertext[i].isupper() and ciphertext[i].isalpha():
+            if (ord(ciphertext[i]) - shift) < ord("A"):
+                plaintext += chr(ord("Z") + 1 - (ord("A") - (ord(ciphertext[i]) - shift)))
+            else:
+                plaintext += chr(ord(ciphertext[i]) - shift)
+        else:
+            plaintext += ciphertext[i]
     return plaintext
