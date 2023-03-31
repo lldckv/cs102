c8a69ca1a0cbf207f01ba8b63d4e6f71783f9fe7
lldckv <lldckv@gmail.com>
2023-03-31 12:04:51 +0300

    what is up with isort

--- homework04/life_console.py
+++ homework04/life_console.py
@@ -7,16 +7,40 @@ from ui import UI
 class Console(UI):
     def __init__(self, life: GameOfLife) -> None:
         super().__init__(life)
+        self.life = life
 
     def draw_borders(self, screen) -> None:
-        """ Отобразить рамку. """
-        pass
+        """Отобразить рамку."""
+        # y, x = screen.getmaxyx()
+        # y-=3
+        # x-=2
+        # border_line = '+' + (x * '-') + '+'
+        # screen.addstr(0, 0, border_line)
+        # screen.addstr(y + 1, 0, border_line)
+        # for i in range(0,y):
+        #   screen.addstr(1 + i, 0, '|')
+        #  screen.addstr(1 + i, x + 1, '|')
+        # screen.refresh()"
+        screen.border("|", "|", "-", "-", "+", "+", "+", "+")
 
     def draw_grid(self, screen) -> None:
-        """ Отобразить состояние клеток. """
-        pass
+        """Отобразить состояние клеток."""
+        grid = self.life.curr_generation
+        for i in range(len(grid)):
+            screen.addstr(i + 1, 1, "".join(map(str, grid[i])).replace("1", "*").replace("0", " "))
 
     def run(self) -> None:
         screen = curses.initscr()
+        # screen.clear()
         # PUT YOUR CODE HERE
+        screen.nodelay(True)
+        screen.resize(self.life.rows + 2, self.life.cols + 2)
+
+        self.draw_borders(screen)
+        while screen.getch() != ord("q"):
+            self.draw_grid(screen)
+            self.life.step()
+            screen.refresh()
+        curses.endwin()
+
         curses.endwin()
