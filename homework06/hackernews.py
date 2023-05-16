818693779a30cc4a5a9652d647d8db032ff18a71
lldckv <lldckv@MacBook-Air-Alla.local>
2023-05-16 19:31:48 +0300

    please fix im crying

--- homework06/hackernews.py
+++ homework06/hackernews.py
@@ -1,7 +1,7 @@
-from bayes import NaiveBayesClassifier
-from bottle import redirect, request, route, run, template
-from db import News, session
-from scraputils import get_news
+from bayes import NaiveBayesClassifier  # type: ignore
+from bottle import redirect, request, route, run, template  # type: ignore
+from db import News, session  # type: ignore
+from scraputils import get_news  # type: ignore
 
 
 @route("/news")
